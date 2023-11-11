e02b6faecb732178f88a75e774e3a688d3d093fd
ScarletRedMan <terentev.andrey.2002@gmail.com>
2023-11-11 12:23:38 +0700

    Initial commit

---
+++ src/main/java/ru/dragonestia/loadbalancer/LoadBalancerApplication.java
@@ -0,0 +1,12 @@
+package ru.dragonestia.loadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancerApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/dragonestia/loadbalancer/LoadBalancerApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.dragonestia.loadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
