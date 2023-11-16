f25ab87b7b36052502ed765194202bda99359be5
ScarletRedMan <terentev.andrey.2002@gmail.com>
2023-11-16 10:27:52 +0700

    Implemented node service

---
+++ LoadBalancerWeb/src/main/java/ru/dragonestia/loadbalancer/web/LoadBalancerWebApplication.java
@@ -0,0 +1,12 @@
+package ru.dragonestia.loadbalancer.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancerWebApplication.class, args);
+    }
+}
--- src/main/java/ru/dragonestia/loadbalancer/service/NodeService.java
+++ src/main/java/ru/dragonestia/loadbalancer/service/NodeService.java
@@ -3,6 +3,7 @@ package ru.dragonestia.loadbalancer.service;
 import ru.dragonestia.loadbalancer.model.Node;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NodeService {
 
@@ -11,4 +12,6 @@ public interface NodeService {
     void removeNode(Node node);
 
     List<Node> allNodes();
+
+    Optional<Node> findNode(String identifier);
 }
