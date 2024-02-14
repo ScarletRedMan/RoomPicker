7a3871e35db80025d2075a0cd53d5db60f81d5cd
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-02-14 23:24:12 +0700

    Documented RoomPicker Info

--- api/src/main/java/ru/dragonestia/picker/api/repository/response/RoomPickerInfoResponse.java
+++ api/src/main/java/ru/dragonestia/picker/api/repository/response/RoomPickerInfoResponse.java
@@ -1,3 +1,8 @@
 package ru.dragonestia.picker.api.repository.response;
 
-public record RoomPickerInfoResponse(String version) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "Server info", hidden = true)
+public record RoomPickerInfoResponse(
+        @Schema(description = "RoomPicker server version", example = "0.0.1") String version
+) {}
