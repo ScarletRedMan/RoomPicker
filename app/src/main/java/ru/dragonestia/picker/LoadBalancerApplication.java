76ee311d2cdb7ffee0d150b78bdd7c479e183ad3
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-02-14 19:49:32 +0700

    Added application info to OpenAPI documentation

--- app/src/main/java/ru/dragonestia/picker/LoadBalancerApplication.java
+++ app/src/main/java/ru/dragonestia/picker/LoadBalancerApplication.java
@@ -1,8 +1,28 @@
 package ru.dragonestia.picker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "RoomPicker Server",
+                version = "0.0.1",
+                description = "Game room picker system and load balancer",
+                license = @License(name = "GPL-3.0", url = "https://github.com/ScarletRedMan/RoomPicker/blob/master/LICENSE"),
+                contact = @Contact(url = "https://github.com/ScarletRedMan", name = "Andrey Terentev", email = "terentev.andrey.2002@gmail.com")
+        ),
+        servers = {
+                @Server(
+                        description = "Local server",
+                        url = "http://localhost:8080"
+                )
+        }
+)
 @SpringBootApplication
 public class LoadBalancerApplication {
 
