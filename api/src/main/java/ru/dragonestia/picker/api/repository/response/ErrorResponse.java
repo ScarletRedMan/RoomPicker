2d1bf1434bfc2e1e2c367db3e49a5a15a8b3bab4
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-02-14 22:07:43 +0700

    Documented Nodes

--- api/src/main/java/ru/dragonestia/picker/api/repository/response/ErrorResponse.java
+++ api/src/main/java/ru/dragonestia/picker/api/repository/response/ErrorResponse.java
@@ -1,5 +1,13 @@
 package ru.dragonestia.picker.api.repository.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.Map;
 
-public record ErrorResponse(String errorId, String message, Map<String, String> details) {}
+@Schema(
+        name = "ErrorResponse",
+        description = "Response with error info"
+)
+public record ErrorResponse(String errorId,
+                            String message,
+                            Map<String, String> details) {}
--- api/src/main/java/ru/dragonestia/picker/api/repository/response/NodeDetailsResponse.java
+++ api/src/main/java/ru/dragonestia/picker/api/repository/response/NodeDetailsResponse.java
@@ -1,5 +1,7 @@
 package ru.dragonestia.picker.api.repository.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import ru.dragonestia.picker.api.repository.response.type.RNode;
 
+@Schema(title = "Node details", hidden = true)
 public record NodeDetailsResponse(RNode node) {}
