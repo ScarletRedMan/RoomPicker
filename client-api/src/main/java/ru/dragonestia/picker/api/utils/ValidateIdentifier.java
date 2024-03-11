9dd79e0c0bbc459d5aad700d276857536e454516
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-03-11 20:28:01 +0700

    Renamed module 'api' to 'client-api'

--- client-api/src/main/java/ru/dragonestia/picker/api/utils/ValidateIdentifier.java
+++ client-api/src/main/java/ru/dragonestia/picker/api/utils/ValidateIdentifier.java
@@ -5,14 +5,14 @@ public class ValidateIdentifier {
     private ValidateIdentifier() {}
 
     public static boolean forNode(String nodeId) {
-        return nodeId.matches("^[a-z\\d-]+$");
+        return nodeId.matches("^(?!-)[a-z\\d-]{0,31}[a-z\\d](?!-)$");
     }
 
     public static boolean forRoom(String roomId) {
-        return roomId.matches("^[a-z\\d-]+$");
+        return roomId.matches("^(?!-)[a-z\\d-]{0,31}[a-z\\d](?!-)$");
     }
 
     public static boolean forUser(String username) {
-        return username.matches("^[aA-zZ\\d-.\\s:@_;]+$");
+        return username.matches("^[aA-zZ\\d-.\\s:@_;]{1,64}$");
     }
 }
