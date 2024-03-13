5a75cf7ff96aefd7764c19811bdc83f9ad7436ee
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-03-13 12:48:43 +0700

    Added getting identifier objects from models

--- client-api/src/main/java/ru/dragonestia/picker/api/model/node/INode.java
+++ client-api/src/main/java/ru/dragonestia/picker/api/model/node/INode.java
@@ -2,10 +2,15 @@ package ru.dragonestia.picker.api.model.node;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import ru.dragonestia.picker.api.repository.type.NodeIdentifier;
 
 public interface INode {
 
     @NotNull String getIdentifier();
+
+    default @NotNull NodeIdentifier getIdentifierObject() {
+        return NodeIdentifier.of(getIdentifier());
+    }
 
     @NotNull PickingMethod getPickingMethod();
 
--- client-api/src/main/java/ru/dragonestia/picker/api/model/user/IUser.java
+++ client-api/src/main/java/ru/dragonestia/picker/api/model/user/IUser.java
@@ -2,10 +2,15 @@ package ru.dragonestia.picker.api.model.user;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import ru.dragonestia.picker.api.repository.type.UserIdentifier;
 
 public interface IUser {
 
     @NotNull String getIdentifier();
+
+    default @NotNull UserIdentifier getIdentifierObject() {
+        return UserIdentifier.of(getIdentifier());
+    }
 
     @Nullable String getDetail(@NotNull UserDetails detail);
 }
