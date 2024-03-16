894d882f9e155034eff6eb9108bc99c18a370430
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-03-16 16:08:32 +0700

    Updated Vaadin files

---
+++ control-panel/frontend/generated/flow/chunks/chunk-0c6ec94efe36378e73adb47fc438174852fa051b44271a0a2847a0db8ff19645.js
@@ -0,0 +1,7 @@
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/password-field/theme/lumo/vaadin-password-field.js';
+import '@vaadin/tooltip/theme/lumo/vaadin-tooltip.js';
+import '@vaadin/button/theme/lumo/vaadin-button.js';
+import 'Frontend/generated/jar-resources/buttonFunctions.js';
+import '@vaadin/text-field/theme/lumo/vaadin-text-field.js';
+import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
