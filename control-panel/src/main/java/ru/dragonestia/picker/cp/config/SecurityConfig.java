158038a6f9b75070fb3fed7785c5800d662c8179
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-03-18 18:30:35 +0700

    Fixed security config

--- control-panel/src/main/java/ru/dragonestia/picker/cp/config/SecurityConfig.java
+++ control-panel/src/main/java/ru/dragonestia/picker/cp/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package ru.dragonestia.picker.cp.config;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.dragonestia.picker.api.impl.RoomPickerClient;
 import ru.dragonestia.picker.cp.model.provider.AccountProvider;
+import ru.dragonestia.picker.cp.page.LoginPage;
 import ru.dragonestia.picker.cp.service.AccountService;
 
 @Configuration
@@ -30,5 +32,15 @@ public class SecurityConfig extends VaadinWebSecurity {
     @Bean
     UserDetailsService userDetailsService(RoomPickerClient adminClient, AccountProvider accountProvider) {
         return new AccountService(adminClient, accountProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> {
+
+        });
+
+        super.configure(http);
+        setLoginView(http, LoginPage.class);
     }
 }
