9c9db591480821dd0d9dcd8fdf527ffac3ccbd83
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-03-25 11:41:22 +0700

    Updated security configuration

--- control-panel/src/main/java/ru/dragonestia/picker/cp/config/SecurityConfig.java
+++ control-panel/src/main/java/ru/dragonestia/picker/cp/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package ru.dragonestia.picker.cp.config;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.dragonestia.picker.api.impl.RoomPickerClient;
-import ru.dragonestia.picker.cp.model.provider.AccountProvider;
 import ru.dragonestia.picker.cp.page.LoginPage;
 import ru.dragonestia.picker.cp.service.AccountService;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends VaadinWebSecurity {
+
+    private AccountService accountService;
+
+    @Autowired
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -31,16 +36,17 @@ public class SecurityConfig extends VaadinWebSecurity {
         };
     }
 
-    @Bean
-    UserDetailsService userDetailsService(RoomPickerClient adminClient, AccountProvider accountProvider) {
-        return new AccountService(adminClient, accountProvider);
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> {
 
         });
+
+        http.formLogin(login -> {
+            login.successForwardUrl("/nodes");
+        });
+
+        http.userDetailsService(accountService);
 
         super.configure(http);
         setLoginView(http, LoginPage.class);
