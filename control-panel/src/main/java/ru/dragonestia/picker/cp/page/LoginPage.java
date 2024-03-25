c77426aedbaeb014f0419da536630389b6322d11
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-03-25 23:49:18 +0700

    Added redirect for '/login' when authenticated

--- control-panel/src/main/java/ru/dragonestia/picker/cp/page/LoginPage.java
+++ control-panel/src/main/java/ru/dragonestia/picker/cp/page/LoginPage.java
@@ -1,23 +1,32 @@
 package ru.dragonestia.picker.cp.page;
 
 import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.router.*;
+import jakarta.annotation.security.PermitAll;
 import lombok.extern.log4j.Log4j2;
+import ru.dragonestia.picker.cp.service.SecurityService;
 
 @Log4j2
-@AnonymousAllowed
+@PermitAll
 @Route("/login")
-public class LoginPage extends VerticalLayout implements BeforeEnterObserver {
+public class LoginPage extends VerticalLayout implements BeforeEnterObserver, AfterNavigationObserver {
 
     private final LoginForm formLogin;
+    private final boolean authenticated;
 
-    public LoginPage() {
+    public LoginPage(SecurityService securityService) {
+        if (securityService.getAuthenticatedAccount() != null) {
+            formLogin = null;
+            authenticated = true;
+            return;
+        }
+
+        authenticated = false;
+
         setAlignItems(Alignment.CENTER);
 
         add(new Html("<h1><u>RoomPicker!</u></h1>"));
@@ -46,5 +55,12 @@ public class LoginPage extends VerticalLayout implements BeforeEnterObserver {
 
             formLogin.setError(true);
         }
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
+        if (!authenticated) return;
+
+        getUI().ifPresent(ui -> ui.navigate("/nodes"));
     }
 }
