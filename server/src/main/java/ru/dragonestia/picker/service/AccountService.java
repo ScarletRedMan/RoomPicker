8cf75e675bca3af67a27ddcebf72cebd358df698
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-03-25 11:45:53 +0700

    Secured AccountService

--- server/src/main/java/ru/dragonestia/picker/service/AccountService.java
+++ server/src/main/java/ru/dragonestia/picker/service/AccountService.java
@@ -1,6 +1,7 @@
 package ru.dragonestia.picker.service;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import ru.dragonestia.picker.model.Account;
 
@@ -8,9 +9,12 @@ import java.util.Collection;
 
 public interface AccountService extends UserDetailsService {
 
+    @PreAuthorize("hasRole('ADMIN')")
     @NotNull Account createNewAccount(@NotNull String username, @NotNull String password);
 
+    @PreAuthorize("hasRole('ADMIN')")
     @NotNull Collection<Account> allAccounts();
 
+    @PreAuthorize("hasRole('ADMIN')")
     void removeAccount(@NotNull Account account);
 }
