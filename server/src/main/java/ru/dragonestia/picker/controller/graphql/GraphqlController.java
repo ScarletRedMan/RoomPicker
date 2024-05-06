96d2941bc68ba79a73cdf3855390a3a0b8e7bb5f
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-05-06 17:17:01 +0700

    added more data for entities

--- server/src/main/java/ru/dragonestia/picker/controller/graphql/GraphqlController.java
+++ server/src/main/java/ru/dragonestia/picker/controller/graphql/GraphqlController.java
@@ -1,9 +1,9 @@
 package ru.dragonestia.picker.controller.graphql;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import ru.dragonestia.picker.controller.graphql.entity.EntityNode;
+import ru.dragonestia.picker.controller.graphql.entity.type.DataProvider;
 import ru.dragonestia.picker.service.NodeService;
 import ru.dragonestia.picker.service.RoomService;
 import ru.dragonestia.picker.service.UserService;
@@ -11,17 +11,24 @@ import ru.dragonestia.picker.service.UserService;
 import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
 public class GraphqlController {
 
     private final NodeService nodeService;
     private final RoomService roomService;
     private final UserService userService;
+    private final DataProvider dataProvider;
+
+    public GraphqlController(NodeService nodeService, RoomService roomService, UserService userService) {
+        this.nodeService = nodeService;
+        this.roomService = roomService;
+        this.userService = userService;
+        dataProvider = new DataProvider(nodeService, roomService, userService);
+    }
 
     @QueryMapping
     List<EntityNode> allNodes() {
         return nodeService.all().stream()
-                .map(EntityNode::new)
+                .map(node -> new EntityNode(node, dataProvider))
                 .toList();
     }
 }
